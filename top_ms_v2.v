e34ec84fecfcab0a57398d7c5fd8c97f4aadf93c
Ruben304 <94462717+Ruben304@users.noreply.github.com>
2022-12-11 15:34:26 -0500

    updated

--- top_ms_v2.v
+++ top_ms_v2.v
@@ -1,16 +1,42 @@
-module top_ms ( clk_i, reset_i, mode_switch, set_hrs, set_min, set_sec, set_time, set_ms, digit_select, b_1, b_2, b_3, b_4, b_5, VGA_HS_O, VGA_VS_O, VGA_R,VGA_G,VGA_B); //buttons 1-5
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/10/2022 07:23:50 PM
+// Design Name: 
+// Module Name: clock_divider_1khz
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
-    input clk_i, reset_i, set_time;
-    input [5:0] set_hrs;
-    input  [4:0] set_min;
-    input  [4:0] set_sec;
-    input [9:0] set_ms;
+module top_ms ( clk_i, reset_i, mode_switch, b_1, VGA_HS_O, VGA_VS_O, VGA_R,VGA_G,VGA_B); //buttons 1-5
+
+    input clk_i, reset_i;
+//   input set_time    
+//    input [5:0] set_hrs;
+//    input  [4:0] set_min;
+//    input  [4:0] set_sec;
+//    input [9:0] set_ms;
     
     input [1:0] mode_switch; //mode switch
-    input b_1, b_2, b_3, b_4, b_5; //buttons2-5 timer
+    input b_1; //buttons2-5 timer
     output VGA_HS_O, VGA_VS_O;
     output [3:0] VGA_R,VGA_G,VGA_B;
-    output reg [2:0] digit_select; //timer switch
+    
+    reg [5:0] set_hrs = 11;
+    reg [4:0] set_min = 35;
+    reg [4:0] set_sec = 42;
+    reg set_time = 0;
     
     wire CD1; //CLOCK DIVIDER 1KHZ, DB
     
@@ -29,9 +55,9 @@ module top_ms ( clk_i, reset_i, mode_switch, set_hrs, set_min, set_sec, set_time
     
     digital_clk_24hr_ms dc2(.clk_i( CD1 ), .reset_i( reset_i ), .Timeset( set_time ), .Hourset( set_hrs ), .Minset( set_min ), .Secset( set_sec ), .sec_o( DC24_SEC ), .min_o( DC24_MIN ), .hour_o( DC24_HRS ), .ms_o(DC24_MS));      
     
-    stopwatch_ms ss2( .clk_i( CD1 ), .reset_i( reset_i ), .start_stop( b_1  ), .Timeset( set_time ), .Hourset( set_hrs ), .Minset( set_min ), .Secset( set_sec  ), .sec_o( SW_SEC  ), .min_o( SW_MIN ), .hour_o( SW_HRS ), .Msset(set_ms), .ms_o(SW_MS) );
+    stopwatch_ms ss2( .clk_i( clk_i), .reset_i( reset_i ), .start_stop( b_1  ), .Timeset( set_time ), .Hourset( set_hrs ), .Minset( set_min ), .Secset( set_sec  ), .sec_o( SW_SEC  ), .min_o( SW_MIN ), .hour_o( SW_HRS ), .Msset(set_ms), .ms_o(SW_MS) );
     
-    timer tt1(.clk_i(clk_i), .reset_i(reset_i), .start_i(b_1), .left_i(b_2), .right_i(b_3), .up_i(b_4), .down_i(b_5), .ml_o(TI_MS), .sec_o(TI_SEC), .min_o(TI_MIN), .hour_o(TI_HRS), .digitp(digit_select));
+    timer ttl(.clk_i( clk_i), .reset_i(reset_i), .start_i(b_1), .ml_o(TI_MS), .sec_o(TI_SEC), .min_o(TI_MIN), .hour_o(TI_HRS));
     
    always @*
         if (mode_switch == 2'b00) begin
@@ -49,13 +75,13 @@ module top_ms ( clk_i, reset_i, mode_switch, set_hrs, set_min, set_sec, set_time
             TS_MIN <= TI_MIN;
             TS_SEC <= TI_SEC;
             TS_MS <= TI_MS;
-        end else if (mode_switch == 2'b10) begin
+        end else if (mode_switch == 2'b11) begin
             TS_HRS <= SW_HRS;
             TS_MIN <= SW_MIN;
             TS_SEC <= SW_SEC;
             TS_MS <= SW_MS;
         end
- 
+        
     top_square ts1(.CLK(clk_i),.RST_BTN(reset_i), .sec(TS_SEC),.min(TS_MIN),.hour(TS_HRS),.ms(TS_MS),.VGA_HS_O(VGA_HS_O),.VGA_VS_O(VGA_VS_O),.VGA_R(VGA_R),.VGA_G(VGA_G),.VGA_B(VGA_B));
     
-endmodule 
+endmodule       
