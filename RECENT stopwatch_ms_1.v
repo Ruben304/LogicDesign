967537e896d99c6a9b8014f4f1a2d30f07933a40
Ruben304 <94462717+Ruben304@users.noreply.github.com>
2022-12-11 23:56:00 -0500

    Update RECENT stopwatch_ms_1.v

--- RECENT stopwatch_ms_1.v
+++ RECENT stopwatch_ms_1.v
@@ -21,7 +21,7 @@
 
 
 module stopwatch_ms_1(
-    input clk_i, reset_i, start_stop,
+    input clk_i, reset_i,start_stop,
     input [4:0] Hourset,
     input [5:0] Minset,
     input [5:0] Secset,
@@ -31,38 +31,30 @@ module stopwatch_ms_1(
     output reg [4:0] hour_o,
     output reg [9:0] ms_o //counting till 999
                  );
-      reg var_1 = 0;
+      //reg var_1 = 0;
             
   always @ (posedge clk_i) begin 
-       if (var_1 == 1) begin
-             ms_o <= ms_o + 1;
+    if (start_stop == 1'b1)begin
+        ms_o <= ms_o + 1;
                 if (ms_o == 999) begin
                  ms_o <= 0;
                  sec_o <= sec_o + 1;
-                if (sec_o == 59) begin //&& ms_o==999begin 
-                    min_o <= min_o + 1;
-                    sec_o <= 0;
-                    if (min_o == 59) begin //&& ms_o==999 begin
-                            hour_o <= hour_o + 1;
-                            min_o <= 0;
+                    if (sec_o == 59) begin //&& ms_o==999begin 
+                        min_o <= min_o + 1;
+                        sec_o <= 0;
+                            if (min_o == 59) begin //&& ms_o==999 begin
+                                hour_o <= hour_o + 1;
+                                 min_o <= 0;
+                             end
                     end
-                 end
                 end
-         end else begin
-         if (start_stop) begin
-            var_1 <= 1;
-         end
-       end
-     end
-             
-always @ (negedge reset_i) begin
-        if(!reset_i)
-            begin 
-            hour_o <= Hourset;
-            min_o <= Minset;
-            sec_o <= Secset;
-            ms_o <=  0; 
-                var_1 <= 0;
-            end
-end          
+//                if (reset_i == 0) begin
+//                    hour_o = Hourset;
+//                     min_o = Minset;
+//                     sec_o = Secset;
+//                      ms_o =  0; 
+//                 end
+     end 
+   end
+     
 endmodule
